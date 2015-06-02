f782b8b32834f80925f7d47026964c79bef81eb9
Paul Gowder <paul.gowder@gmail.com>
2015-06-02 12:15:19 -0400

    got regex to match dash blocks, but now changing, planning out new, better way

--- autocite.py
+++ autocite.py
@@ -5,6 +5,35 @@ import sys, re, argparse, string
 # save it all as ascii first, no need to worry about unicode so long as conversions to ascii are same in each file 
 # this means this script need not be written for python 3 after all, but what the hell, it's done now.
 
+# actually needs to be able to handle, but not match, unicode characters for greek text in notes. 
+# I think that should be ok... except for foreign authors.  it will break on foreign authors?
+# christ I may have to figure out unicode.
+# this stackoverflow may have answer: 
+# http://stackoverflow.com/questions/6005459/is-there-a-way-to-match-any-unicode-non-alphabetic-character
+
+# ALTERNATIVELY I can just run some damned command-line tool to convert all to ascii first.
+# also, bad news, it looks like I don't have consistent use of accents and such between text 
+# and refs.  this will produce spurious non-matches.  Still, as long as they are rare, 
+# can be fixed by hand.
+
+# converting to ascii seems like clearly best choice.  so long as the things that look like 
+# letters in unicode convert to letters in ascii that should work.
+
+# there appears to be a gnu app that can handle this: 
+# http://www.gnu.org/savannah-checkouts/gnu/libiconv/documentation/libiconv-1.13/iconv.1.html
+
+# the problem with this solution is that if I just replace non-ascii chars with arbitrary 
+# letters, then the stupid MS word dashes will also be replaced by letters, which will 
+# break the dedash function. Probably the easiest solution to that little glitch is to 
+# manually search and replace the dash strings.  
+
+# actually, why not just search and replace the dash strings in the script??  After all, 
+# python can handle unicode, it's only the regex character matching that's an issue. Then 
+# the dedash regex can just match on whatever arbitrary string I put in place of the 
+# dashes. And then I can convert everything else to ascii before doing the real regex 
+# with str.encode.  Ok, that's the plan.  it's dirty but it will work.  Snapshotting a 
+# version here with the regex that matches the stupid dashes first. 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("citearg", help="the file containing the citations from footnotes etc.")
 parser.add_argument("refarg", help="the file containing reference list")
@@ -32,17 +61,22 @@ def dedash(reffchunk):
 		ref = name + year
 		dashedlist.append(ref)
 		sublist = dedash(choppedrefs[1])
-		dashlist.append(sublist)
-		# gonna need another function to take this list of lists and turn it into a list
-		# of strings.  or can I just append the contents of the sublist rather than 
-		# the list itself??  contents must be arbitrary length.
+		dashlist.extend(sublist)
+
 	
 
-# search downward through string to find line starting with non-char.
+# search downward through string to find first reference line starting with non-char.
 # needs to return a two item-list, first item is index of line break before char 
 # after dashblock, as an integer; second is year.  
 # needs to just return integer 0 if nothing is found.
 def goSearchDashes(refchunk): 
+	dashpattern = r'(^[^A-Za-z.]*\.).*( \(?\d\d\d\d[a-z]?[.)])'
+	founddash = re.compile(dashpattern, re.MULTILINE)
+	firstmatch = founddash.search()
+	if firstmatch == None: 
+		return 0
+	else: 
+		year = firstmatch.group(2)
 
 # needs to return a two-item list, first item being string before index, second item being
 # string after index and this second string must start with a newline.  
